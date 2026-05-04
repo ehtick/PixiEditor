e1852bb36959348318727051ae8965e68c9e3973
Krzysztof Krysiński <flubbet@gmail.com>
2026-05-04 11:54:39 +0200

    Fixed binding modifiers

--- src/PixiEditor/Views/Shortcuts/KeyCombinationBox.axaml.cs
+++ src/PixiEditor/Views/Shortcuts/KeyCombinationBox.axaml.cs
@@ -81,7 +81,8 @@ internal partial class KeyCombinationBox : UserControl
 
         if (GetModifier(e.Key) is { } modifier)
         {
-            currentCombination = currentCombination with { Modifiers = currentCombination.Modifiers ^ modifier };
+            currentCombination = currentCombination with { Modifiers = currentCombination.Modifiers ^ modifier, Key = e.Key};
+            KeyCombination = currentCombination;
         }
         else
         {
